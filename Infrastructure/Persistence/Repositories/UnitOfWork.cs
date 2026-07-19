b5fc45e71d9e095ef04ab3f44684a35ec8488ad2
Omar Osama <omarosama534@gmail.com>
2026-07-19 15:53:54 +0300

    Create and Implement UnitOfWork

---
+++ Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using Persistence.Data;
+
+namespace Persistence.Repositories
+{
+    public class UnitOfWork(StoreDbContext _dbContext) : IUnitOFWork
+    {
+        private readonly Dictionary<string, object> repositories = [];
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+            var typeName = typeof(TEntity).Name;
+            if (repositories.ContainsKey(typeName)) return (IGenericRepository<TEntity, TKey>)repositories[typeName];
+
+            else
+            {
+                var repo = new GenericRepository<TEntity, TKey>(_dbContext);
+                repositories["typeName"] = repo;
+                return repo;
+            }
+        }
+
+        public async Task<int> SaveChangesAsync() => await _dbContext.SaveChangesAsync();
+    }
+}
