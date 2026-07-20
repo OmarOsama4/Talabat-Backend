34bfd9a0111395b0a33387ae9c4a08013ed629f1
Omar Osama <omarosama534@gmail.com>
2026-07-20 16:48:27 +0300

    Create and Implement Product Controller

---
+++ Infrastructure/Presentation/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.DataTransferObjects;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController(IServiceManager serviceManager) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetAllProducts()
+        {
+            var Products = await serviceManager.ProductService.GetProductsAsync();
+            return Ok(Products);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ProductDTO>> GetProduc(int id)
+        {
+            var Product = await serviceManager.ProductService.GetProductByIdAsync(id);
+            return Ok(Product);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IEnumerable<TypeDTO>>> GetTypes()
+        {
+            return Ok(await serviceManager.ProductService.GetAllTypesAsync());
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IEnumerable<BrandDTO>>> GetBrands()
+        {
+            return Ok(await serviceManager.ProductService.GetAllBrandsAsync());
+        }
+    }
+}
