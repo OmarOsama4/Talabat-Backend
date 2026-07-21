2bbae8cae70912cc8fc09ebccdaffbbfb378f16e
Omar Osama <omarosama534@gmail.com>
2026-07-21 11:14:53 +0300

    Use Specification design pattern in order to create dynamic query

---
+++ Core/Service/Specifications/BaseSpecifications.cs
@@ -0,0 +1,19 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using System.Linq.Expressions;
+
+namespace Service.Specifications
+{
+    abstract public class BaseSpecifications<TEntity, TKey> : ISpecifications<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        protected BaseSpecifications(Expression<Func<TEntity, bool>> criteriaExp)
+        {
+            Criteria = criteriaExp;
+        }
+        public Expression<Func<TEntity, bool>> Criteria { get; private set; }
+
+        public List<Expression<Func<TEntity, object>>> IncludeExpressions { get; } = [];
+
+        protected void AddInclude(Expression<Func<TEntity, object>> includeExp) => IncludeExpressions.Add(includeExp);
+    }
+}
---
+++ Infrastructure/Persistence/SpecificationEvaluator.cs
@@ -0,0 +1,29 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    static class SpecificationEvaluator
+    {
+        public static IQueryable<TEntity> CreateQuery<TEntity, TKey>(IQueryable<TEntity> inputQuery, ISpecifications<TEntity, TKey> specifications)  where TEntity : BaseEntity<TKey>
+        {
+            var Query = inputQuery; 
+
+            if (specifications.Criteria != null)
+            {
+                Query = Query.Where(specifications.Criteria);
+            }
+
+            if (specifications.IncludeExpressions != null && specifications.IncludeExpressions.Count>0)
+            {
+                foreach (var includeExpression in specifications.IncludeExpressions)
+                {
+                    Query = Query.Include(includeExpression);
+                }
+            }
+
+            return Query;
+        }
+    }
+}
