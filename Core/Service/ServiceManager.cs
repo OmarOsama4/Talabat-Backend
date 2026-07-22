8b13c395bc8930c6facc28210c53580d25271200
Omar Osama <omarosama534@gmail.com>
2026-07-23 00:08:57 +0300

    Add Basket Service in the Service manager

--- Core/Service/ServiceManager.cs
+++ Core/Service/ServiceManager.cs
@@ -5,9 +5,13 @@ using ServiceAbstraction;
 namespace Service
 {
     public class ServiceManager(IUnitOfWork unitOfWork,
+        IBasketRepository basketRepository,
         IMapper mapper) : IServiceManager
     {
         private readonly Lazy<IProductService> _LazyproductService = new Lazy<IProductService>(() => new ProductService(unitOfWork, mapper));
         public IProductService ProductService => _LazyproductService.Value;
+
+        private readonly Lazy<IBasketService> _LazyBasketService = new Lazy<IBasketService>(() => new BasketService(basketRepository,mapper));
+        public IBasketService BasketService => _LazyBasketService.Value;
     }
 }
