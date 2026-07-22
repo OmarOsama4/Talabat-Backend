7067a742fd961706cf1f61182723491d112eb1d6
Omar Osama <omarosama534@gmail.com>
2026-07-22 23:32:08 +0300

    Create and Implement Basket Repository and apply redis connection

---
+++ Core/DomainLayer/Contracts/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using DomainLayer.Models.BasketModel;
+
+namespace DomainLayer.Contracts
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket?> GetBasketAsync(string key);
+        Task<CustomerBasket?> CreateOrUpdateBasketAsync(CustomerBasket basket, TimeSpan? TimeToLive=null);
+        Task<bool> DeleteBasketAsync(string id);
+    }
+} 
---
+++ Infrastructure/Persistence/Repositories/BasketRepository.cs
@@ -0,0 +1,32 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models.BasketModel;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace Persistence.Repositories
+{
+    public class BasketRepository(IConnectionMultiplexer connection) : IBasketRepository
+    {
+        private readonly IDatabase _database = connection.GetDatabase();
+        public async Task<CustomerBasket?> CreateOrUpdateBasketAsync(CustomerBasket basket, TimeSpan? TimeToLive = null)
+        {
+            var JsonBasket = JsonSerializer.Serialize(basket);
+            var IsCreated = await _database.StringSetAsync(basket.Id, JsonBasket, TimeToLive ?? TimeSpan.FromDays(30));
+            if (IsCreated)
+                return await GetBasketAsync(basket.Id);
+            else
+                return null;
+        }
+
+        public async Task<bool> DeleteBasketAsync(string id) => await _database.KeyDeleteAsync(id);
+
+        public async Task<CustomerBasket?> GetBasketAsync(string key)
+        {
+            var Basket = await _database.StringGetAsync(key);
+            if (Basket.IsNullOrEmpty)
+                return null;
+            else 
+                return JsonSerializer.Deserialize<CustomerBasket>(Basket!);
+        }
+    }
+}
