cb021cde59b0cb0885177bf2bd90a45d141bb729
Omar Osama <omarosama534@gmail.com>
2026-07-23 23:11:54 +0300

    Create and implement Authenticatio Controller for the login and registration

--- Core/DomainLayer/Exceptions/BadRequestException.cs
+++ Core/DomainLayer/Exceptions/BadRequestException.cs
@@ -1,6 +1,6 @@
 ﻿namespace DomainLayer.Exceptions
 {
-    public sealed class BadRequestException(List<string> errors) : Exception("Validation Errors Occurred"))
+    public sealed class BadRequestException(List<string> errors) : Exception("Validation Errors Occurred")
     {
         public List<string> Errors { get; } = errors;
     }
---
+++ Infrastructure/Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.DataTransferObjects.IdentityDTO;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthenticationController(IServiceManager serviceManager) : ControllerBase
+    {
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
+        {
+            var user = await serviceManager.AuthenticationService.LoginAsync(loginDTO);
+            return Ok(user);
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDTO)
+        {
+            var user = await serviceManager.AuthenticationService.RegisterAsync(registerDTO);
+            return Ok(user);
+        }
+
+    }
+}
