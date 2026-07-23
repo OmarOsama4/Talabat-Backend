ae738e034dc454a8f7ad5d878f011e1e6a5ea270
Omar Osama <omarosama534@gmail.com>
2026-07-24 02:23:18 +0300

    Create the signature of the authentication service

--- Core/ServiceAbstraction/IAuthenticationService.cs
+++ Core/ServiceAbstraction/IAuthenticationService.cs
@@ -6,5 +6,9 @@ namespace ServiceAbstraction
     {
         Task<UserDTO> RegisterAsync(RegisterDTO registerDTO);
         Task<UserDTO> LoginAsync(LoginDTO loginDTO);
+        Task<bool> CheckEmailAsync(string email);
+        Task<AddressDTO> GetUserAddressAsync(string email);
+        Task<AddressDTO> UpdateUserAddressAsync(string email);
+        Task<UserDTO> GetUserAsync(string email);
     }
 }
