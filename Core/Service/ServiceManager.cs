bd0676f9a3b15723e038077b3466adb5fed64700
Omar Osama <omarosama534@gmail.com>
2026-07-23 23:04:58 +0300

    Add authentication service into the service manager

--- Core/Service/ServiceManager.cs
+++ Core/Service/ServiceManager.cs
@@ -1,17 +1,23 @@
 ﻿using AutoMapper;
 using DomainLayer.Contracts;
+using DomainLayer.Models.IdentityModule;
+using Microsoft.AspNetCore.Identity;
 using ServiceAbstraction;
 
 namespace Service
 {
     public class ServiceManager(IUnitOfWork unitOfWork,
         IBasketRepository basketRepository,
-        IMapper mapper) : IServiceManager
+        IMapper mapper,
+        UserManager<ApplicationUser> userManager) : IServiceManager
     {
         private readonly Lazy<IProductService> _LazyproductService = new Lazy<IProductService>(() => new ProductService(unitOfWork, mapper));
-        public IProductService ProductService => _LazyproductService.Value;
-
         private readonly Lazy<IBasketService> _LazyBasketService = new Lazy<IBasketService>(() => new BasketService(basketRepository,mapper));
+        private readonly Lazy<IAuthenticationService> _LazyAuthenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(userManager));
+
+        public IProductService ProductService => _LazyproductService.Value;
         public IBasketService BasketService => _LazyBasketService.Value;
+        public IAuthenticationService AuthenticationService =>_LazyAuthenticationService.Value;
+
     }
 }
