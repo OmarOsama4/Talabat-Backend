ed95b77ceb5f7295122e4ec8be75a7fdbb8ab390
Omar Osama <omarosama534@gmail.com>
2026-07-25 02:02:31 +0300

    Implement the remaining order endpoints

--- Infrastructure/Presentation/Controllers/OrdersController.cs
+++ Infrastructure/Presentation/Controllers/OrdersController.cs
@@ -18,6 +18,29 @@ namespace Presentation.Controllers
             return Ok(Order);
         }
 
+        [HttpGet("DeliveryMethods")]
+        public async Task<ActionResult<DeliveryMethodDTO>> GetDeliveryMethods()
+        {
+            var methods = await serviceManager.OrderService.GetDeliveryMethodsAsync();
+            return Ok(methods);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderToReturnDTO>>> GetAllOrders()
+        {
+            var Orders = await serviceManager.OrderService.GetAllOrdersAsync(GetEmailFromToken());
+            return Ok(Orders);
+        }
+
+        [Authorize]
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<OrderToReturnDTO>> GetOrderById(Guid id)
+        {
+            var Order = await serviceManager.OrderService.GetOrderByIdAsync(id);
+            return Ok(Order);
+        }
+
         private string GetEmailFromToken()
         {
             return User.FindFirstValue(ClaimTypes.Email)!;
