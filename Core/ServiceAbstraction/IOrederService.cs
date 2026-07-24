04215e3f487fdb05086b9a10c38af675709f4462
Omar Osama <omarosama534@gmail.com>
2026-07-24 18:43:11 +0300

    Create Order DTO for the orderService

---
+++ Core/ServiceAbstraction/IOrederService.cs
@@ -0,0 +1,7 @@
+﻿namespace ServiceAbstraction
+{
+    public interface IOrederService
+    {
+
+    }
+}
---
+++ Shared/DataTransferObjects/OrderModuleDTO/OrderDTO.cs
@@ -0,0 +1,11 @@
+﻿using Shared.DataTransferObjects.IdentityDTO;
+
+namespace Shared.DataTransferObjects.OrderModuleDTO
+{
+    public class OrderDTO
+    {
+        public string BasketId { get; set; } = default!;
+        public int DeliveryMethodId { get; set; }
+        public AddressDTO Address { get; set; } = default!;
+    }
+}
