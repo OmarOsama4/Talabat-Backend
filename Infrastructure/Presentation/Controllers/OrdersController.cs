63d0da868094441c2ce73d71d9154f042b08c7ec
Omar Osama <omarosama534@gmail.com>
2026-07-24 22:46:35 +0300

    Implement Create Order Endpoint

---
+++ Infrastructure/Presentation/Controllers/OrdersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.DataTransferObjects.OrderModuleDTO;
+using System.Security.Claims;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class OrdersController(IServiceManager serviceManager) : ControllerBase
+    {
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<OrderToReturnDTO>> CreateOrder(OrderDTO orderDTO)
+        {
+            var Order = await serviceManager.OrderService.CreateOrder(orderDTO, GetEmailFromToken());
+            return Ok(Order);
+        }
+
+        private string GetEmailFromToken()
+        {
+            return User.FindFirstValue(ClaimTypes.Email)!;
+        }
+    }
+}
