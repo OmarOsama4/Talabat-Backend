19472aa1fcb1ff510cec63ab4f1fe3fa7331334f
Omar Osama <omarosama534@gmail.com>
2026-07-24 22:49:11 +0300

    Fix Column Name

---
+++ Infrastructure/Persistence/Data/Migrations/20260724194809_RenameColumnToProduct_PictureUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumnToProduct_PictureUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Product_PictureUel",
+                table: "OrderItems",
+                newName: "Product_PictureUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Product_PictureUrl",
+                table: "OrderItems",
+                newName: "Product_PictureUel");
+        }
+    }
+}
