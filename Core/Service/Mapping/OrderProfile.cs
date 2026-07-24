2ecfa4a6a1e718e3a65c28b12fa9bab13d35ad05
Omar Osama <omarosama534@gmail.com>
2026-07-24 22:27:18 +0300

    Create mapping profiles for the order

--- Core/Service/Mapping/OrderProfile.cs
+++ Core/Service/Mapping/OrderProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using DomainLayer.Models.OrderModule;
 using Shared.DataTransferObjects.IdentityDTO;
+using Shared.DataTransferObjects.OrderModuleDTO;
 
 namespace Service.Mapping
 {
@@ -8,7 +9,14 @@ namespace Service.Mapping
     {
         public OrderProfile()
         {
-            CreateMap<AddressDTO, OrderAddress>();
+            CreateMap<AddressDTO, OrderAddress>().ReverseMap();
+
+            CreateMap<Order, OrderToReturnDTO>()
+                .ForMember(D => D.DeliveryMethod, o => o.MapFrom(S => S.DeliveryMethod.ShortName));
+
+            CreateMap<OrderItem, OrderItemDTO>()
+                .ForMember(D => D.ProductName, o => o.MapFrom(s => s.Product.ProductName))
+                .ForMember(D => D.PictureUrl, o => o.MapFrom<OrderItemPictureUrlResolver>());
         }
     }
 }
