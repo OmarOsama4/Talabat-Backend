36beb7290c48a1dec095697ecd93c509062747ad
Omar Osama <omarosama534@gmail.com>
2026-07-25 20:32:16 +0300

    Add PaymentsController and integrate PaymentService
    Introduce PaymentsController with an endpoint to create or update payment intents for baskets. Update ServiceManager and IServiceManager to include and expose the new PaymentService, enabling payment operations through the API. Endpoint is secured with authorization and returns updated basket data.

--- Core/Service/ServiceManager.cs
+++ Core/Service/ServiceManager.cs
@@ -17,10 +17,13 @@ namespace Service
         private readonly Lazy<IBasketService> _LazyBasketService = new Lazy<IBasketService>(() => new BasketService(basketRepository,mapper));
         private readonly Lazy<IAuthenticationService> _LazyAuthenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(userManager, configuration, mapper));
         private readonly Lazy<IOrderService> _LazyOrderService = new Lazy<IOrderService>(() => new OrderService(mapper,basketRepository,unitOfWork));
+        private readonly Lazy<IPaymentService> _LazyPaymentService = new Lazy<IPaymentService>(() => new PaymentService(configuration,basketRepository,unitOfWork,mapper));
+
 
         public IProductService ProductService => _LazyproductService.Value;
         public IBasketService BasketService => _LazyBasketService.Value;
         public IAuthenticationService AuthenticationService =>_LazyAuthenticationService.Value;
         public IOrderService OrderService => _LazyOrderService.Value;
+        public IPaymentService PaymentService => _LazyPaymentService.Value;
     }
 }
---
+++ Infrastructure/Presentation/Controllers/PaymentsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.DataTransferObjects.BasketModuleDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class PaymentsController(IServiceManager serviceManager) : ControllerBase
+    {
+        [Authorize]
+        [HttpPost("{BasketId}")]
+        public async Task<ActionResult<BasketDTO>> CreateOrUpdatePaymentIntent(string BasketId)
+        {
+            var Basket = await serviceManager.PaymentService.CreateOrUpdatePaymentIntentAsync(BasketId);
+            return Ok(Basket);
+        }
+
+    }
+}
